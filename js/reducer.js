e11726803e4aae72f4b15dc3ec26a03430efc0e3
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-11-27 13:15:24 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,23 +1,27 @@
 let state;
- 
-function changeState(state = { count: 0 }, action) {
-    switch (action.type) {
- 
-      case 'INCREASE_COUNT':
-        return { count: state.count + 1 }
- 
-      default:
-        return state;
-    }
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
   }
+}
  
-  function dispatch(action){
-  state = changeState(state, action)
-  render()
+function dispatch(action){
+  state = reducer(state, action);
+  render();
 }
  
 function render(){
-  document.body.textContent = state.count
+  let container = document.getElementById('container');
+  container.textContent = state.count;
 }
  
 dispatch({type: '@@INIT'})
+ 
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
