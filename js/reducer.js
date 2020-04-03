ee9e7229c01157c173c368b7fde7f723b9824622
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-04-03 10:53:45 -0700

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,6 +1,11 @@
-let state = {count: 0};
+// Let's begin by simply declaring our state, but not assigning it to equal anything.
+// So, we accordingly change the first line of our JavaScript:
+let state; // = {count: 0};
 
-function changeState(state, action){
+// What would be really nice is if we could say when you pass a state of undefined to our reducer,
+// assign that value to our initial state. Luckily, ES6 allows us to pass default arguments to functions
+// and we can give our changeState() reducer a default argument to do just that.
+function changeState(state = { count: 0 }, action) {
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
