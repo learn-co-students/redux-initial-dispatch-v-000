49f12028dcc2905c94eb4cc65b8207065fcfda00
github username <github email address>
2020-09-23 11:23:53 -0700

    Done.

---
+++ js/new.js
@@ -0,0 +1,23 @@
+let state = {count: 0}
+
+
+function changeState(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
+  }
+}
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+
+
+function render(){
+  document.body.textContent = state.count
+}
+
+dispatch({ type: '@@INIT' })
